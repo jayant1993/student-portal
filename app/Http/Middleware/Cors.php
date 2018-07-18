43604b9a045ccd2872315e306e398ddd20e53ab0
jayant <n.jayant7@gmail.com>
2018-07-19 00:36:13 +0530

    added cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,10 +16,8 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-        ->header('Access-Control-Allow-Origin', 'http://localhost:4200')
+        ->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-        ->header('Access-Control-Allow-Credentials', 'true')
-        ->header('Access-Control-Expose-Headers', 'Set-Cookie');
+        ->header('Access-Control-Allow-Headers', 'Content-Type');
     }
 }
